0042fc5619ebf68bf771cee704f367a3484db663
Grena-tax <grena.tax.ge@gmail.com>
2025-11-17 18:33:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -251,3 +251,5 @@
     markFxTable();
   }
 })();
+<!-- すでに入っているGoogle翻訳の公式スクリプトはそのままでOK -->
+<!-- <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> -->
